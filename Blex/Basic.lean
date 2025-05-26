4152b1e899053653fcd20c90a6f7fbd46076455e
JJoaoll <joaoduos@gmail.com>
2025-05-25 21:11:27 -0300

    update

--- Blex/Basic.lean
+++ Blex/Basic.lean
@@ -9,7 +9,8 @@ def hatter : List (List α) → α → List (List α)
 
 #eval [ [[2], [3]], [[2, 3]] ].map (hatter · 1)
 
-
+def Prod.mapSnd (w : α × β) (f : β → γ) : α × γ :=
+  ⟨w.1, f w.2⟩
 
 instance : Applicative List where
   pure x := [x]
@@ -52,9 +53,75 @@ def fitsIn (str : String) (rx : Regex) : Bool :=
   | klosr rx =>
     let strx := str.data |> Utils.allCuts |> (List.map ∘ List.map) String.mk
     strx.any (·.all (fitsIn · rx))
+
+open Regex
+
+def r! (str : String) : Regex :=
+  match str.data with
+  | [] => Regex.ε
+  | [c] => Regex.char c
+  | c::cs => Regex.ε
+
 end Regex
 
 namespace NFA
+-- Gambiarra total!
+structure NFA where
+  balls : List String              -- representam as calls
+  edges : List $ List (Char × Nat)
+deriving Repr, Inhabited
+-- a lista na porta 0 representa uma seta vinda do nada.
+-- a lista na porta 1 representa uma seta saindo de 1.
+
+#eval [1, 2, 3].dropLast ++ [[1, 2, 3].getLast!]
+def tst := [[1,2,3], [4,5,6], [7,8,9]]
+#eval [[1,2,3], [4,5,6], [7,8,9]].dropLast ++ [0 :: tst.getLast!]
+
+open Regex
+open Regex in
+def fromRegex : Regex → NFA
+  | char c => ⟨[""], [[(c, 0)]]⟩
+  | ε => ⟨[""], [[('_', 0)]]⟩
+  | altrn rx₁ rx₂ =>
+    let M := fromRegex rx₁
+    let N := fromRegex rx₂
+
+    let M_ := M.edges.map (·.map (·.map id (· + 1)))
+    let N_ := N.edges.map (·.map (·.map id (· + 1 + M.balls.length)))
+
+    let t_len := M.balls.length + N.balls.length + 2 - 1
+
+    NFA.mk
+      ("" :: (M.balls ++ N.balls ++ [""]))
+      ([('∈', 0)] -- coming from no-where
+      :: M_.dropLast ++ [('∈', t_len) :: M_.getLast!]
+      ++ N_.dropLast ++ [('∈', t_len) :: N_.getLast!]
+      )
+  | conct rx₁ rx₂ =>
+    let M := fromRegex rx₁
+    let N := fromRegex rx₂
+
+    NFA.mk (M.balls ++ N.balls)
+           (M.edges ++ N.edges)
+  | klosr rx =>
+    let M := fromRegex rx
+    let new_ending := M.balls.length + 1
+
+    NFA.mk
+      (M.balls ++ [""])
+      ([('∈', new_ending)] :: M.edges.drop 1 ++ [[('∈', new_ending)]] ++ M.edges.take 1)
+
+
+
+
+
+-- inductive Regex where
+--   | ε
+--   | char (c : Char)
+--   | altrn (rx₁ rx₂ : Regex)
+--   | conct (rx₁ rx₂ : Regex)
+--   | klosr (rx : Regex)
+
 
 
 end NFA
